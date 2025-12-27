a47585e4e3803459d405075436f4c7e1957b9d8d
minji2219 <aabb0219@naver.com>
2025-12-27 14:33:40 +0900

    chore: css 초기화

--- apps/frontend/src/styles/theme.ts
+++ apps/frontend/src/styles/theme.ts
@@ -23,9 +23,18 @@ const COLOR = {
   },
 }
 
+const SPACING = {
+  xs: 4,
+  sm: 12,
+  md: 16,
+  lg: 24,
+  xl: 32,
+}
+
 export const THEME = {
   COLOR,
   FONTS,
+  SPACING,
 }
 
 export type CustomTheme = typeof THEME
